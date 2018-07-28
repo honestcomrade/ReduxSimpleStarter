4fbec6676881416f9137b6f879d573034013a9c5
Joe Saraceno <honestcomrade@gmail.com>
2018-07-27 22:15:37 -0700

    revert

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-import promise from 'redux-promise';
 
 import NewPost from './components/new-post';
 import PostDetails from './components/post-details';
 import PostsIndex from './components/posts-index';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
