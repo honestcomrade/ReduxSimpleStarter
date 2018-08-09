81ef01b7a9b559271dbb6f238db98c8269467ff0
Joe Saraceno <honestcomrade@gmail.com>
2018-08-08 18:07:26 -0700

    form submit

--- src/components/views/new-post.js
+++ src/components/views/new-post.js
@@ -1,19 +1,71 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class NewPost extends Component {
   render() {
+    const { handleSubmit } = this.props;
     return (
-      <div>
-        <h3>Submit a new post.</h3>
-        <form action="submitPost">
-          <label htmlFor="post-name">Post Name</label>
-          <input type="text" className="form-group" id="post-name" />
-          <label htmlFor="post-content">Content</label>
-          <textarea type="text" className="form-group" id="post-content" />
-        </form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          name="title"
+          Label="Title"
+          type="text"
+          component={this.renderField}
+        />
+        <Field
+          name="categories"
+          Label="Categories"
+          type="text"
+          component={this.renderField}
+        />
+        <Field
+          name="content"
+          Label="Content"
+          type="text"
+          component={this.renderField}
+        />
+        <Field
+          name="print"
+          Label="Print"
+          type="checkbox"
+          component={this.renderField}
+        />
+        <button 
+          className="btn btn-success" 
+          type="submit"
+          >Submit
+        </button>
+      </form>
+    );
+  };
+  onSubmit(values) {
+    console.log(values);
+  }
+  renderField(field) {
+    return (
+      <div className="form-group">
+      <label htmlFor="">{field.Label}</label>
+        <input className="form-control"
+          type={field.type}
+          {...field.input}
+        />
+        <p>{field.meta.error}</p>
       </div>
     );
-  }
+  };
+};
+
+const validate = (values) => {
+  const errors = {};
+  for (const prop in values) {
+    if (!values[prop]) {
+      errors[prop] = `Missing value for ${prop}`;
+    }
+  };
+  return errors;
 }
 
-export default NewPost;
+export default reduxForm({
+  form: 'PostsNewForm',
+  validate,
+})(NewPost);
--- src/components/views/posts-index.js
+++ src/components/views/posts-index.js
@@ -1,15 +1,18 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect  } from "react-redux";
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../../actions';
 
 class PostsIndex extends Component {
   renderPosts() {
     return _.map(this.props.posts, post => {
       return (
-        <li className="list-group-item" key={ post.id }>
-          { post.title }
-        </li>
+        <Link to="/post/post._id" key={ post.id }>
+          <li className="list-group-item">
+            { post.title }
+          </li>
+        </Link>
       );
     });
   };
@@ -21,6 +24,13 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
+        <div className="text-xs-right">
+          <Link 
+            className="btn btn-primary"
+            to="/posts/new">
+              Submit a new post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           {this.renderPosts()}
